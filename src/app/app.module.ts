8e745a4290633a57df1dd946fe07639165fd4380
kyle-gilmartin <kylegilmartin8@gmail.com>
2021-02-22 18:48:41 +0000

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,13 +17,13 @@ import { HomeComponent } from './home/home.component'
 
 export const firebaseConfig = {
  
-  apiKey: "AIzaSyDMGvMuOAbqztSzo7gq6okKEDeIuk_BXmw",
-  authDomain: "locationapp-9a780.firebaseapp.com",
-  projectId: "locationapp-9a780",
-  storageBucket: "locationapp-9a780.appspot.com",
-  messagingSenderId: "372934126302",
-  appId: "1:372934126302:web:f2486d72196ccca23fb04f",
-  measurementId: "G-7RM9W3TT9C"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 
 };
 
